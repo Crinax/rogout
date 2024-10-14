6866ac26eadbca52c03863433d71fbf13df251e4
Crinax <agreedeth@gmail.com>
2024-10-14 23:03:17 +0300

    feat(commands): complete commands

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,20 +1,18 @@
-use std::process::Command;
+use std::{os::unix::process::CommandExt, process::Command};
 
 #[tauri::command]
 pub fn system_shutdown() {
-  println!("Shutting down...");
+    Command::new("poweroff").exec();
 }
 
 #[tauri::command]
 pub fn system_restart() {
-  println!("Restarting...");
+    Command::new("reboot").exec();
 }
 
 #[tauri::command]
 pub fn system_suspend() -> bool {
-  let output = Command::new("systemctl")
-    .arg("suspend")
-    .output();
+    let output = Command::new("systemctl").arg("suspend").output();
 
-  return output.is_ok();
+    return output.is_ok();
 }
